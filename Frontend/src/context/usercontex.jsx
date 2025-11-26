9d3899320cc43482e35eb9986f0144edaa7a90f5
Gantavya Bansal <gantavyaoo@gmail.com>
2025-11-27 00:51:37 +0530

    Remove trailing slash from server URL

--- Frontend/src/context/usercontex.jsx
+++ Frontend/src/context/usercontex.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const dataContext = createContext();
 
 function UserContext({ children }) {
-        const serverUrl = "https://crimzer.onrender.com/";
+        const serverUrl = "https://crimzer.onrender.com";
         const [currentUser, setCurrentUser] = useState(null);
 
             const fetchCurrentUser = useCallback(async () => {
